304f246364e465139e6dc1ae553dabbdae253e0a
holoprogram <2488070031@qq.com>
2022-07-21 15:53:21 +0800

    my answer to the function 3 of Program Assignment 1

---
+++ corr.R
@@ -0,0 +1,28 @@
+## My answer to the function 3
+
+corr <- function(directory, threshold = 0){
+        ## 'threshold' is a numeric vector of 1 indicating the
+        ## number of completely observed observations(on all
+        ## variables) required to compute the correlation between
+        ## nitrate and sulfate; the default is 0
+        
+        ## Return a numeric vector of correlations
+        ## NOTE: Do not round the result!
+        ## (e.g., if threshold is 150, then 
+        ## the condition on each file is having at least 151 complete cases)
+        files_full <- list.files(directory, full.names = TRUE, pattern = "csv")
+        dat <- data.frame()
+        r <- vector()
+        for(i in 1:332){
+                dtread <- read.csv(files_full[i])
+                nobs <- sum(complete.cases(dtread))
+                if(nobs > threshold){
+                        my_cor <- cor(dtread[,'sulfate'], dtread[, 'nitrate'], "complete")
+                        # result
+                        r <- c(r, my_cor)
+                }
+        }
+        r
+}
+
+# get some changes to push the code to github
