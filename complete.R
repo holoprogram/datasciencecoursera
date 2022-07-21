3e5111d64c5f23b28c4e3cb7635358898d2755a5
holoprogram <2488070031@qq.com>
2022-07-21 11:03:54 +0800

    my answer to the function 2 of the program assignment 1

---
+++ complete.R
@@ -0,0 +1,31 @@
+
+complete <- function(directory, id=1:332){
+        ## 'directory' is a character vector of length 1
+        ## indicating the location of the CSV files
+        
+        ## 'id' is an integer vector indicating the monitor ID numbers
+        ## to be used
+        
+        ## Return a data frame of the form:
+        ## id nobs
+        ## 1  117
+        ## 2  1041
+        ## ...
+        ## where 'id' is the monitor ID number and 'nobs' is the 
+        ## number of complete cases
+        
+        files_full <- list.files(directory, full.names = TRUE, pattern = "csv")
+        dat <- data.frame()
+        for(i in id){
+                dtread <- read.csv(files_full[i])
+                c <- dtread[, 'sulfate']
+                gn <- !is.na(dtread[, 'sulfate'])
+                r <- c[gn]
+                dat <- rbind(dat,c(i,length(r)))
+        }
+        
+        ## set colnames
+        colnames(dat) <- c('id', 'nobs')
+        dat
+
+}
